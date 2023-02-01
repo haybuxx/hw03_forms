873b4b7fd0ca3492c4d41f41edef99ca73981af2
Rinat Khaibullin <rinatkhaibullin@MacBook-Pro-Rinat.local>
2023-02-01 18:51:00 +0900

    Ревью

--- yatube/posts/utils.py
+++ yatube/posts/utils.py
@@ -2,6 +2,7 @@ from django.core.paginator import Paginator
 from django.http import HttpRequest
 from django.db.models import QuerySet
 
+
 def get_context_page(request: HttpRequest, queryset: QuerySet, pages: int):
     paginator = Paginator(queryset, pages)
     page_number = request.GET.get('page')
